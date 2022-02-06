5f3ee3d60b7d1003ebfce11787847f059075b3e8
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-02-06 15:58:38 +1100

    Update script.js to include function that take in an operator and two numbers and prints out the result.

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 function add(...inputs)
 {
-    return inputs.reduce((total, element) => total+element, 0);
+    return inputs.reduce((total, element) => total + +element, 0);
 }
 
 function subtract(...inputs)
 {
-    return inputs.reduce((total, element) => total - element, inputs[0]*2);
+    return inputs.reduce((total, element) => total - +element, inputs[0]*2);
 }
 
 function multiply(...inputs)
 {
-    return inputs.reduce((total, element) => total * element, 1);
+    return inputs.reduce((total, element) => total * +element, 1);
 }
 
 function divide(...inputs)
 {
-    return inputs.reduce((total, element) => total/element, inputs[0]*inputs[0]);
+    return inputs.reduce((total, element) => total/+element, inputs[0]*inputs[0]);
+}
+
+function operate(...inputs)
+{
+    if(inputs.length == 1)
+    {
+        inputs = Array.from(inputs[0]);
+    }
+
+    const operator = inputs.find(element => {return element.match(/[+\-*\/]/) != null});
+    inputs = inputs.join("").split(operator);
+
+    switch(operator)
+    {
+        case '+':
+            return add(inputs[0], inputs[1]);
+        case '-':
+            return subtract(inputs[0], inputs[1]);
+        case '*':
+            return multiply(inputs[0], inputs[1]);
+        case '/':
+            return divide(inputs[0], inputs[1]);
+    }
 }
